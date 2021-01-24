7c8831771b1bd0ea9f34963c26267976f9b5b57a
Dariusz Okonski <darek200180@gmail.com>
2021-01-24 23:35:43 +0000

    refactoring, change names to more appropriate

--- engine.py
+++ engine.py
@@ -1,17 +1,17 @@
 from constants import BRICK, SPACE, GATE
 from ui import display_board
-from levels import build_board_level_one, build_board_level_two, build_board_level_three
+from levels import build_board_content_central, build_board_content_left, build_board_content_right
 
 LEFT_MAIN_DOOR_ROW = 2
 LEFT_MAIN_DOOR_COL = 0
-RIGHT_MAIN_DOOR_ROW = 3
+RIGHT_MAIN_DOOR_ROW = 8
 
 def build_an_empty_board(width, height):
     board = []
     for _ in range(height):
         board.append([SPACE] * width)
 
-    build_bricks_wall_around_board(board)
+    board = build_bricks_wall_around_board(board)
 
     return board
 
@@ -24,6 +24,7 @@ def build_bricks_wall_around_board(board):
                 board[i][j] = BRICK
             elif j == 0 or j == width - 1:
                 board[i][j] = BRICK
+    return board
 
 def check_if_change_board(player, board):
     width = len(board[0])
@@ -31,7 +32,7 @@ def check_if_change_board(player, board):
     #change to board two, left gate at board one
     if player['row'] == LEFT_MAIN_DOOR_ROW and player['col'] == LEFT_MAIN_DOOR_COL:
         board = build_an_empty_board(width, height)
-        board = build_gates_at_sides_level_two(board)
+        board = build_gates_at_left_board(board)
         player['row'] = LEFT_MAIN_DOOR_ROW
         player['col'] = width - 2
         return player, board
@@ -39,56 +40,54 @@ def check_if_change_board(player, board):
     # change to board one, right gate at board two
     if player['row'] == LEFT_MAIN_DOOR_ROW and player['col'] == width - 1:
         board = build_an_empty_board(width, height)
-        board = build_gates_at_sides_level_one(board)
+        board = build_gates_at_central_board(board)
         player['row'] = LEFT_MAIN_DOOR_ROW
         player['col'] = LEFT_MAIN_DOOR_COL + 1
         return player, board
-
+    # change to board three, right gate at board one
     if player['row'] == RIGHT_MAIN_DOOR_ROW and player['col'] == width -1:
         board = build_an_empty_board(width, height)
-        board = build_gates_at_sides_level_three(board)
+        board = build_gates_at_right_board(board)
         player['row'] = RIGHT_MAIN_DOOR_ROW
         player['col'] = LEFT_MAIN_DOOR_COL + 1
         return player, board
 
+    # change to board one, left gate at board three
     if player['row'] == RIGHT_MAIN_DOOR_ROW and player['col'] == LEFT_MAIN_DOOR_COL:
         board = build_an_empty_board(width, height)
-        board = build_gates_at_sides_level_one(board)
+        board = build_gates_at_central_board(board)
         player['row'] = RIGHT_MAIN_DOOR_ROW
         player['col'] = width - 2
         return player, board
 
     return player, board
 
-def build_gates_at_sides_level_one(board):
+def build_gates_at_central_board(board):
     width = len(board[0])
     height = len(board)
     board = build_an_empty_board(width, height)
 
     board[LEFT_MAIN_DOOR_ROW][LEFT_MAIN_DOOR_COL] = GATE
     board[RIGHT_MAIN_DOOR_ROW][width - 1] = GATE
-    # board[LEFT_MAIN_DOOR_ROW][width - 1] = BRICK
 
-    build_board_level_one(board)
+    build_board_content_central(board)
     return board
 
-def build_gates_at_sides_level_two(board):
+def build_gates_at_left_board(board):
     width = len(board[0])
     height = len(board)
     board = build_an_empty_board(width, height)
 
     board[LEFT_MAIN_DOOR_ROW][width - 1] = GATE
-    # board[LEFT_MAIN_DOOR_ROW][LEFT_MAIN_DOOR_COL] = BRICK
-    # board[RIGHT_MAIN_DOOR_ROW][width - 1] = BRICK
-    build_board_level_two(board)
+    build_board_content_left(board)
     return board
 
-def build_gates_at_sides_level_three(board):
+def build_gates_at_right_board(board):
     width = len(board[0])
     height = len(board)
     board = build_an_empty_board(width, height)
     board[RIGHT_MAIN_DOOR_ROW][0] = GATE
-    build_board_level_three(board)
+    build_board_content_right(board)
     return board
 
 def clear_player_previous_position(board, row, col):
@@ -102,8 +101,7 @@ def can_player_move(board, row, col):
 
 def create_board(width=5, height=5):
     board = build_an_empty_board(width, height)
-    # build_bricks_wall_around_board(board)
-    board = build_gates_at_sides_level_one(board)
+    board = build_gates_at_central_board(board)
 
     return board
 
--- levels.py
+++ levels.py
@@ -1,20 +1,20 @@
-from  constants import BRICK
+from constants import BRICK
 
-def build_board_level_one(board):
+def build_board_content_central(board):
     board[5][5] = BRICK
     board[6][5] = BRICK
     board[7][5] = BRICK
     board[8][5] = BRICK
     return board
 
-def build_board_level_two(board):
+def build_board_content_left(board):
     board[5][18] = BRICK
     board[6][8] = BRICK
     board[7][8] = BRICK
     board[8][8] = BRICK
     return board
 
-def build_board_level_three(board):
+def build_board_content_right(board):
     board[5][8] = BRICK
     board[5][9] = BRICK
     board[5][10] = BRICK
